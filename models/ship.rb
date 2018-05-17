943c18ecf9088eec0ae53205ff61c8af7eacdea7
michael hohl <mhohl@rocketmail.com>
2018-05-17 23:16:25 +0000

    automatically backed up by learn

--- models/ship.rb
+++ models/ship.rb
@@ -1,16 +1,21 @@
 class Ship
-  attr_reader :name, :type, :booty
+  attr_accessor :name, :type, :booty
 
-  SHIPS = []
+  @@all = []
 
   def initialize(args)
     @name = args[:name]
     @type = args[:type]
     @booty = args[:booty]
-    SHIPS << self
+    @@all << self
   end
 
   def self.all
-    SHIPS
+    @@all
   end
+
+  def self.clear
+    @@all = []
+  end
+
 end
